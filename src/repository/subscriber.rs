f8fdf6d6825944699cd984d0154b0ec8b86f8890
raviehasan <ravie.hasan@ui.ac.id>
2024-04-17 11:47:19 +0700

    Implement list_all function in Subscriber repository.

--- src/repository/subscriber.rs
+++ src/repository/subscriber.rs
@@ -1,6 +1,6 @@
+use crate::model::subscriber::Subscriber;
 use dashmap::DashMap;
 use lazy_static::lazy_static;
-use crate::model::subscriber::Subscriber;
 
 lazy_static! {
     static ref SUBSRICBERS: Dashmap<String, Dashmap<String, Subscriber>> = Dashmap::new();
@@ -9,15 +9,29 @@ lazy_static! {
 pub struct SubscriberRepository;
 
 impl SubscriberRepository {
-    
     pub fn add(product_type: &str, subscriber: Subscriber) -> Subscriber {
         let subscriber_value = subscriber.clone();
         if SUBSRICBERS.get(product_type).is_none() {
             SUBSRICBERS.insert(String::from(product_type), DashMap::new());
         };
 
-        SUBSRICBERS.get(product_type).unwrap()
+        SUBSRICBERS
+            .get(product_type)
+            .unwrap()
             .insert(subscriber_value.url.clone(), subscriber_value);
         return subscriber;
+    }
+
+    pub fn list_all(product_type: &str) -> Vec<Subscriber> {
+        if SUBSRICBERS.get(product_type).is_none() {
+            SUBSRICBERS.insert(String::from(product_type), DashMap::new());
+        };
+
+        return SUBSRICBERS
+            .get(product_type)
+            .unwrap()
+            .iter()
+            .map(|f| f.value().clone())
+            .collect();
     }
 }
