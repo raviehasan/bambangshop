abcb066c10c165fc8c72e1864f3cc8fd891fdd12
raviehasan <ravie.hasan@ui.ac.id>
2024-04-17 16:03:37 +0700

    Edit Product service methods to call notify after create/delete.

--- src/service/product.rs
+++ src/service/product.rs
@@ -1,9 +1,10 @@
 use rocket::http::Status;
 use rocket::serde::json::Json;
 
-use bambangshop::{Result, compose_error_response};
 use crate::model::product::Product;
 use crate::repository::product::ProductRepository;
+use crate::service::notification::NotificationService;
+use bambangshop::{compose_error_response, Result};
 
 pub struct ProductService;
 
@@ -11,6 +12,12 @@ impl ProductService {
     pub fn create(mut product: Product) -> Result<Product> {
         product.product_type = product.product_type.to_uppercase();
         let product_result: Product = ProductRepository::add(product);
+
+        NotificationService.notify(
+            &product_result.product_type,
+            "CREATED",
+            product_result.clone(),
+        );
 
         return Ok(product_result);
     }
@@ -24,7 +31,7 @@ impl ProductService {
         if product_opt.is_none() {
             return Err(compose_error_response(
                 Status::NotFound,
-                String::from("Product not found.")
+                String::from("Product not found."),
             ));
         }
         return Ok(product_opt.unwrap());
@@ -35,18 +42,22 @@ impl ProductService {
         if product_opt.is_none() {
             return Err(compose_error_response(
                 Status::NotFound,
-                String::from("Product not found.")
+                String::from("Product not found."),
             ));
         }
         let product: Product = product_opt.unwrap();
 
+        NotificationService.notify(&product.product_type, "DELETED", product.clone());
         return Ok(Json::from(product));
     }
 
     pub fn publish(id: usize) -> Result<Product> {
         let product_opt: Option<Product> = ProductRepository::get_by_id(id);
         if product_opt.is_none() {
-            return Err(compose_error_response(Status::NotFound, String::from("Product not found")));
+            return Err(compose_error_response(
+                Status::NotFound,
+                String::from("Product not found"),
+            ));
         }
         let product: Product = product_opt.unwrap();
 
